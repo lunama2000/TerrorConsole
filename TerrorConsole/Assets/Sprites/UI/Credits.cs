60a177688ff7fff84839dc571f1a6b32dfdda6f2
Sinkablepeak4 <101901592+Sinkablepeak@users.noreply.github.com>
2025-03-06 18:34:10 -0600

    nose

--- TerrorConsole/Assets/Sprites/UI/Credits.cs
+++ TerrorConsole/Assets/Sprites/UI/Credits.cs
@@ -4,6 +4,18 @@ namespace TerrorConsole
 {
     public class Credits : MonoBehaviour
     {
-        
+        public float scrollSpeed = 10f;
+        public RectTransform rectTransform;
+
+        void Start()
+        {
+            rectTransform = GetComponent<RectTransform>();
+        }
+
+        void Update ()
+        {
+            rectTransform.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
+        }
+            
     }
 }
