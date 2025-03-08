ca3e9ef48f05945759ed557b54bea4fe39d8bb04
Sinkablepeak4 <101901592+Sinkablepeak@users.noreply.github.com>
2025-03-08 16:24:22 -0600

    Loop de creditos Completo
    👍

--- TerrorConsole/Assets/Sprites/UI/Credits.cs
+++ TerrorConsole/Assets/Sprites/UI/Credits.cs
@@ -6,24 +6,25 @@ namespace TerrorConsole
     {
         public float scrollSpeed = 10f;
         public RectTransform rectTransform;
-        private float startPositionY;
-
+        private float startPositionY; // Guardamos la posición inicial en Y
 
         void Start()
         {
-            startPositionY = rectTransform.anchoredPosition.y;
             rectTransform = GetComponent<RectTransform>();
+            startPositionY = rectTransform.anchoredPosition.y; // Guardamos la posición inicial
         }
 
-        void Update ()
+        void Update()
         {
+            // Movemos el cuadro de texto hacia arriba
             rectTransform.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
 
+            // Si el cuadro de texto sale de la pantalla por la parte superior
             if (rectTransform.anchoredPosition.y > Screen.height)
             {
-                rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, -Screen.height);
+                // Lo reposicionamos al inicio (en la parte inferior de la pantalla)
+                rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, startPositionY);
             }
         }
-
     }
 }
