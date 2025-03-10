602585d6a400183cb05abe874d4652b5d0b8078b
Chilo <zap520@amerike.edu.mx>
2025-03-10 02:15:13 -0600

    Animation Dialogue
    Dialogue box with fade animation and animated every character in a sentence using DOTween

--- TerrorConsole/Assets/_Main/Scripts/Dialogue/DialogueManager.cs
+++ TerrorConsole/Assets/_Main/Scripts/Dialogue/DialogueManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
 using UnityEngine.Rendering;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
@@ -11,21 +12,21 @@ namespace TerrorConsole
     public class DialogueManager : MonoBehaviour
     {
         
-        [FormerlySerializedAs("dialogueCanvas")]
+        [FormerlySerializedAs("_dialogueCanvas")]
         [Header("Components")]
-        [SerializeField] private Canvas _dialogueCanvas; 
-        [SerializeField] private GameObject dialogueBox; 
+        [SerializeField] private CanvasGroup dialogueCanvas; 
         [SerializeField] private TextMeshProUGUI dialogueText;
         
         [Header("Configuration")]
         [SerializeField] private float dialogueDuration;
-        [SerializeField] private float dialogueAnimationDuration;
+        [SerializeField] private float dialogueAnimationDuration = 0.5f;
+        [SerializeField] private float dialogueTextSpeed = 6f;
         
-        private Queue<string> sentences;
+        private Queue<string> _sentences;
 
         void Start() 
         {
-            sentences = new Queue<string>();
+            _sentences = new Queue<string>();
         }
         
         /*public void Transition(Action action, DialogueTransitionType transitionType)
@@ -54,32 +55,46 @@ namespace TerrorConsole
         }*/
         public void StartDialogue(DialogueData dialogueData)
         {
-            _dialogueCanvas.gameObject.SetActive(true);
-            sentences.Clear();
-
-            foreach (string sentence in dialogueData.sentences)
+            dialogueCanvas.alpha = 0;
+            dialogueCanvas.gameObject.SetActive(true);
+            dialogueCanvas.DOFade(1f, dialogueAnimationDuration);//.OnComplete(() =>
             {
-                sentences.Enqueue(sentence);
-            }
-            NextSentence();
+                _sentences.Clear();
+                foreach (string sentence in dialogueData.sentences)
+                {
+                    _sentences.Enqueue(sentence);
+                }
+                NextSentence();
+            }//);
         }
         
         
         public void NextSentence()
         {
-            if (sentences.Count == 0)
+            if (_sentences.Count == 0)
             {
                 EndDialogue();
                 return;
             }
             
-            string sentence = sentences.Dequeue();
-            dialogueText.text = sentence;
+            string sentence = _sentences.Dequeue();
+            AnimateText(sentence);
         }
 
+        private void AnimateText(string sentence)
+        {
+            string text = "";
+            DOTween.To(() => text, x => text = x, sentence, sentence.Length / dialogueTextSpeed).OnUpdate(() =>
+            {
+                dialogueText.text = text;
+            });
+            
+        }
+        
         void EndDialogue()
         {
-            dialogueBox.SetActive(false);
+            dialogueCanvas.DOFade(0f, dialogueAnimationDuration)
+                .OnComplete(() => dialogueCanvas.gameObject.SetActive(false));
         }
         
         public enum DialogueTransitionType
