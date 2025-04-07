e0244747f69de71be3c3905ac0a076b799965ffc
Sinkablepeak4 <101901592+Sinkablepeak@users.noreply.github.com>
2025-04-06 19:17:44 -0600

    Controles de UI (Trabajo en proceso 70%)

--- TerrorConsole/Assets/_Main/Scripts/UI/UIController.cs
+++ TerrorConsole/Assets/_Main/Scripts/UI/UIController.cs
@@ -1,27 +1,142 @@
+using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TerrorConsole
 {
     public class UIController : MonoBehaviour
     {
-        public void ButtonLoadScene(string sceneName)
+        [SerializeField] private RectTransform _selector;
+        [SerializeField] private RectTransform[] _referenceObject;
+        [SerializeField] private Transform[] _optionsItems;
+        [SerializeField] private Transform[] _menuItems;
+        private Transform[] _currentArrayInUse;
+        //private Image[] optionsImages;
+        int _currentSelection = 0;
+
+        bool _allowedToSellect = false;
+
+        CUREENT_MENU_STATE _currentState = CUREENT_MENU_STATE.MAIN_MENU;
+        [SerializeField] GameObject _optionsScreen;
+
+        enum CUREENT_MENU_STATE
         {
-            ScreenTransitionManager.Source.TransitionToScene(sceneName);
+            MAIN_MENU,
+            OPTIONS
         }
 
-        public void ButtonPauseLevel()
+        void Start()
         {
-            LevelManager.Source.PauseLevel();
+            _currentArrayInUse = _menuItems;
+            _allowedToSellect = false;
+            _currentSelection = 0;
+            ChangeSelectorPosition();
+        }
+        void ChangeSelectorPosition()
+        { 
+            _selector.SetParent(_currentArrayInUse[_currentSelection]);
+            if (_currentState == CUREENT_MENU_STATE.MAIN_MENU)
+            {
+                _selector.anchoredPosition = new Vector2(-70, 8);
+                _selector.sizeDelta = new Vector2(30,30);
+            }
+            else if (_currentState == CUREENT_MENU_STATE.OPTIONS)
+            {
+                _selector.anchoredPosition = new Vector2(0, 8);
+                _selector.sizeDelta = new Vector2(30, 30);
+            }
+
         }
 
-        public void ButtonResumeLevel()
+        void ChangeCurrentSelection(bool _add = true)
         {
-            LevelManager.Source.PlayLevel();
+
+            if (_add)
+            {
+                _currentSelection++;
+                if (_currentSelection >= _currentArrayInUse.Length)
+                {
+                    _currentSelection = 0;
+                }
+            }
+            else
+            {
+                _currentSelection--;
+                if (_currentSelection < 0)
+                {
+                    _currentSelection = _currentArrayInUse.Length - 1;
+                }
+            }
         }
 
-        public void ButtonPlaySFX()
+        void ChangedCurrentSelectionOptionIsFound(bool _add = true)
         {
-            AudioManager.Source.PlayUIButtonClickSFX();
+            ChangeCurrentSelection(_add);
+
+            while (!_currentArrayInUse[_currentSelection].gameObject.activeInHierarchy)
+            {
+                ChangeCurrentSelection(_add);
+            }
+            ChangeSelectorPosition();
         }
-    }
+
+        void Update()
+        {
+            switch (_currentState)
+            {
+                case CUREENT_MENU_STATE.MAIN_MENU:
+                    if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+                    {
+                        ChangedCurrentSelectionOptionIsFound();
+                    }
+                    else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+                    {
+                        ChangedCurrentSelectionOptionIsFound(false);
+                    }
+                    else if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        if (_currentSelection == 4)
+                        {
+                            Button boton = _currentArrayInUse[_currentSelection].GetComponent<Button>();
+                            _currentState = CUREENT_MENU_STATE.OPTIONS;
+                            _currentSelection = 0;
+                            _currentArrayInUse = _optionsItems;
+                            ChangeSelectorPosition();
+
+                            if (boton != null)
+                            {
+                                boton.onClick.Invoke();
+                            }
+                        }
+                        /*else if (currentSelection == 1)
+                        {
+                            
+                        }*/
+                    }
+                    break;
+                }
+            }
+
+            public void ButtonLoadScene(string sceneName)
+            {
+                ScreenTransitionManager.Source.TransitionToScene(sceneName);
+            }
+
+            public void ButtonPauseLevel()
+            {
+                LevelManager.Source.PauseLevel();
+            }
+
+            public void ButtonResumeLevel()
+            {
+                LevelManager.Source.PlayLevel();
+            }
+
+            public void ButtonPlaySFX()
+            {
+                AudioManager.Source.PlayUIButtonClickSFX();
+            }
+        }
 }
