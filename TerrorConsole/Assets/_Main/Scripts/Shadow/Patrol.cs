850b14e8f2bfe74219fb9012d717d24607653623
Sinkablepeak4 <101901592+Sinkablepeak@users.noreply.github.com>
2025-04-10 22:04:21 -0600

    Avance persecución

--- TerrorConsole/Assets/_Main/Scripts/Shadow/Patrol.cs
+++ TerrorConsole/Assets/_Main/Scripts/Shadow/Patrol.cs
@@ -5,44 +5,87 @@ namespace TerrorConsole
 {
     public class Patrol : MonoBehaviour
     {
-        [SerializeField] private float waitTime;
-        [SerializeField] private float startWaitTime;
+        [Header("Patroll")]
+        [SerializeField] private float _waitTime;
+        [SerializeField] private float _startWaitTime = 3f;
+        [SerializeField] Transform[] _spots;
+        private NavMeshAgent _agent;
+        private int _randomSpots;
 
-        [SerializeField] Transform[] spots;
-        private NavMeshAgent agent;
-        private int randomSpots;
+        [Header("Chase")]
+        [SerializeField] private float _searchCone;
+        private LayerMask _player;
+        private Transform _playerTransform;
+
+        StatesEnemy _currentState = StatesEnemy.PATROL;
+
+        public enum StatesEnemy 
+        {
+            PATROL,
+            CHASE,
+            WAIT
+        }
 
         private void Awake()
         {
-            waitTime = startWaitTime;
-            randomSpots = Random.Range(0, spots.Length);
-            agent = GetComponent<NavMeshAgent>();
-            agent.updateRotation = false;
-            agent.updateUpAxis = false;
+            _waitTime = _startWaitTime;
+            _randomSpots = Random.Range(0, _spots.Length);
+            _agent = GetComponent<NavMeshAgent>();
+            _agent.updateRotation = false;
+            _agent.updateUpAxis = false;
         }
 
         void DestinationRandomSpot()
         {
-            agent.SetDestination(spots[randomSpots].position); 
+            _agent.SetDestination(_spots[_randomSpots].position); 
         }
 
         void WaitTimeAndDistance()
         {
-            if (Vector2.Distance(transform.position, spots[randomSpots].position) < 0.2f)
+            _currentState = StatesEnemy.PATROL;
+
+            if (Vector2.Distance(transform.position, _spots[_randomSpots].position) < 0.2f)
             {
-                if (waitTime <= 0)
+                if (_waitTime <= 0)
                 {
-                    waitTime = startWaitTime;
-                    randomSpots = Random.Range(0, spots.Length);
+                    _currentState = StatesEnemy.WAIT;
+                    _waitTime = _startWaitTime;
+                    _randomSpots = Random.Range(0, _spots.Length);
                 }
                 else
                 {
-                    waitTime -= Time.deltaTime;
+                    _waitTime -= Time.deltaTime;
                 }
             }
         }
 
-        private void Update () 
+        void ChasingPlayer()
+        {
+            _currentState = StatesEnemy.CHASE;
+            Collider2D PlayerCollider = Physics2D.OverlapCircle(transform.position, _searchCone, _player);
+            if (PlayerCollider != null)
+            {
+                _playerTransform = PlayerCollider.transform;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, _searchCone);
+        }
+
+        void WaitAfterChase()
+        {
+            _currentState = StatesEnemy.WAIT;
+        }
+
+        void BackToPatroll()
+        {
+            WaitTimeAndDistance();
+        }
+
+        private void FixedUpdate ()
         {
             DestinationRandomSpot();
             WaitTimeAndDistance();
