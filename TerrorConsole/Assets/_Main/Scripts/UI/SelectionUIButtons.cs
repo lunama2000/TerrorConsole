0673ab3d78995e1ad81802ec966e3af8e8af19fb
Sinkablepeak <101901592+Sinkablepeak@users.noreply.github.com>
2025-04-10 23:27:16 -0600

    Control game's UI using gamepad (#107)
    * Cambios estéticos necesarios
    
    * Cambios a sprites Controllers y Loading icon
    
    * Controles de UI (Trabajo en proceso 70%)
    
    * 71%
    
    * UI Controllers 100% Complete
    
    :)
    
    * Fixed conflicts
    
    * Everything working
    
    ---------
    
    Co-authored-by: Ivan Herrera <ivan.herrera.m@outlook.com>

---
+++ TerrorConsole/Assets/_Main/Scripts/UI/SelectionUIButtons.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace TerrorConsole
+{
+    public class SelectionUIButtons : MonoBehaviour
+    {
+
+        [SerializeField] private GameObject _menuUIButtons;
+        [SerializeField] private GameObject _optionsUIButtons;
+        [SerializeField] private GameObject _newGameUIButtons;
+
+
+
+
+
+    }
+}
--- TerrorConsole/Assets/_Main/Scripts/UI/UIController.cs
+++ TerrorConsole/Assets/_Main/Scripts/UI/UIController.cs
@@ -1,27 +1,57 @@
+using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
 
 namespace TerrorConsole
 {
+
     public class UIController : MonoBehaviour
     {
+        [SerializeField] private GameObject[] _menuUIButtons;
+        [SerializeField] private GameObject[] _optionsUIButtons;
+        [SerializeField] private GameObject[] _newGameUIButtons;
+
+        public void MenuUIButtons()
+            {
+                EventSystem.current.SetSelectedGameObject(_menuUIButtons[1]);
+            }
+
+        public void OptionsUIButtons()
+            {
+                EventSystem.current.SetSelectedGameObject(_optionsUIButtons[0]);
+            }
+        public void NewGameUIButtons()
+            {
+                EventSystem.current.SetSelectedGameObject(_newGameUIButtons[0]);
+            }
+
+        public void AllMenusClosed()
+            {
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+
         public void ButtonLoadScene(string sceneName)
-        {
-            ScreenTransitionManager.Source.TransitionToScene(sceneName);
-        }
+            {
+                ScreenTransitionManager.Source.TransitionToScene(sceneName);
+            }
 
-        public void ButtonPauseLevel()
-        {
-            LevelManager.Source.PauseLevel();
-        }
+            public void ButtonPauseLevel()
+            {
+                LevelManager.Source.PauseLevel();
+            }
 
-        public void ButtonResumeLevel()
-        {
-            LevelManager.Source.PlayLevel();
-        }
+            public void ButtonResumeLevel()
+            {
+                LevelManager.Source.PlayLevel();
+            }
 
-        public void ButtonPlaySFX()
-        {
-            AudioManager.Source.PlayUIButtonClickSFX();
+            public void ButtonPlaySFX()
+            {
+                AudioManager.Source.PlayUIButtonClickSFX();
+            }
         }
-    }
 }
