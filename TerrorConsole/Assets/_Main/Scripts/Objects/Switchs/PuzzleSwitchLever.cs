d57976f4416a983c5c2d1c45fccb3d683f364ede
Chilo <zap520@amerike.edu.mx>
2025-04-11 16:08:27 -0600

    puzzle levers scripts added

---
+++ TerrorConsole/Assets/_Main/Scripts/Objects/Switchs/PuzzleSwitchLever.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace TerrorConsole
+{
+    public class PuzzleSwitchLever : SwitchObject
+    {
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                InputManager.Source.OnActivateButton1 += AlternateState;
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                InputManager.Source.OnActivateButton1 -= AlternateState;
+            }
+        }
+
+        protected override void AlternateState()
+        {
+            _state = !_state;
+            if (_state)
+            {
+                
+                OnActivated?.Invoke(_id);
+                CameraSystemManager.Source.ShakeCamera();
+            }
+            else
+            {
+                
+                OnDeactivated?.Invoke(_id);
+            }
+        }
+    }
+}
