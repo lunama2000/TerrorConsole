45a2f1675d88972c1c711fdc93c61da012909e78
Ivan Herrera <ivan.herrera.m@outlook.com>
2025-04-19 00:28:05 -0600

    Added cone detection

--- TerrorConsole/Assets/_Main/Scripts/Shadow/Patrol.cs
+++ TerrorConsole/Assets/_Main/Scripts/Shadow/Patrol.cs
@@ -5,26 +5,27 @@ namespace TerrorConsole
 {
     public partial class Patrol : MonoBehaviour
     {
+        [Header("Components")]
+        [SerializeField] private NavMeshAgent _agent;
+        
         [Header("Patrol")]
         [SerializeField] private float _waitTime = 0;
         [SerializeField] private float _startWaitTime = 3f;
         [SerializeField] Transform[] _spots;
 
         [Header("Chase")]
-        [SerializeField] private float _searchCone;
+        [SerializeField] private float _radius = 5;
+        [SerializeField] private float _searchCone = 45;
         [SerializeField] private LayerMask _player;
         [SerializeField] private Transform _playerTransform;
         [SerializeField]private bool _isChasing = false;
         [SerializeField] private Vector2 _lastPlayerPosition;
-        
-        private NavMeshAgent _agent;
         private int _randomSpots;
 
         private void Awake()
         {
             _waitTime = _startWaitTime;
             NewSpot();
-            _agent = GetComponent<NavMeshAgent>();
             _agent.updateRotation = false;
             _agent.updateUpAxis = false;
         }
@@ -60,7 +61,7 @@ namespace TerrorConsole
 
         private void ChasingPlayer()
         {
-            Collider2D playerCollider = Physics2D.OverlapCircle(transform.position, _searchCone, _player);
+            Collider2D playerCollider = DetectPlayerInCone();
             
             if (playerCollider != null)
             {
@@ -113,6 +114,21 @@ namespace TerrorConsole
             }
         }
 
+        private Collider2D DetectPlayerInCone()
+        {
+            var position = transform.position;
+            var hit = Physics2D.OverlapCircle(position, _radius, _player);
+
+            if (hit == null) return null;
+            
+            var directionToPlayer = (hit.transform.position - position).normalized;
+            var directionToDestination = (_agent.steeringTarget - position).normalized;
+            
+            var angleToTarget = Vector2.Angle(directionToDestination, directionToPlayer);
+
+            return angleToTarget <= _searchCone ? hit : null;
+        }
+
         private bool HasReachedDestination(Vector3 destinationPosition)
         {
             return Vector2.Distance(transform.position, destinationPosition) < 0.2f;
@@ -129,8 +145,21 @@ namespace TerrorConsole
     {
         private void OnDrawGizmos()
         {
+            var position = transform.position;  
+            var forward =  (_agent.steeringTarget - position).normalized;
+            var angle = _searchCone;
+
+            var leftRayRotation = Quaternion.AngleAxis(-angle, Vector3.forward);
+            var rightRayRotation = Quaternion.AngleAxis(angle, Vector3.forward);    
+
+            var leftDirection = leftRayRotation * forward;
+            var rightDirection = rightRayRotation * forward;
+
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, _searchCone);
+            Gizmos.DrawWireSphere(position, _radius);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(position, position + leftDirection * _radius);
+            Gizmos.DrawLine(position, position + rightDirection * _radius);
         }
     }
 #endif
