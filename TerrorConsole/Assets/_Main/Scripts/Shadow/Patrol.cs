ac1c60c0529e623384561876840278805b277a54
Sinkablepeak <101901592+Sinkablepeak@users.noreply.github.com>
2025-04-23 14:14:42 -0600

    Enemy follows player (#113)
    * Avance persecución
    
    * Chase shadow incomplete
    
    * Chasing and Back Patroll Complete
    
    * Refactored code for readability
    
    * Added cone detection
    
    * Adjusted thresholds and direction of the shadow patrol
    
    * Adding prefab Shadow
    
    ---------
    
    Co-authored-by: Ivan Herrera <ivan.herrera.m@outlook.com>

--- TerrorConsole/Assets/_Main/Scripts/Shadow/Patrol.cs
+++ TerrorConsole/Assets/_Main/Scripts/Shadow/Patrol.cs
@@ -3,49 +3,172 @@ using UnityEngine.AI;
 
 namespace TerrorConsole
 {
-    public class Patrol : MonoBehaviour
+    public partial class Patrol : MonoBehaviour
     {
-        [SerializeField] private float waitTime;
-        [SerializeField] private float startWaitTime;
+        [Header("Components")]
+        [SerializeField] private NavMeshAgent _agent;
+        
+        [Header("Patrol")]
+        [SerializeField] private float _waitTime = 0;
+        [SerializeField] private float _startWaitTime = 3f;
+        [SerializeField] Transform[] _spots;
 
-        [SerializeField] Transform[] spots;
-        private NavMeshAgent agent;
-        private int randomSpots;
+        [Header("Chase")]
+        [SerializeField] private float _radius = 5;
+        [SerializeField] private float _setTargetThreshold = 0.005f;
+        [SerializeField] private float _searchCone = 45;
+        [SerializeField] private LayerMask _player;
+        [SerializeField] private Transform _playerTransform;
+        [SerializeField]private bool _isChasing = false;
+        [SerializeField] private Vector2 _lastPlayerPosition;
+        
+        private int _randomSpots;
+        private Vector2 _currentViewDirection;
 
         private void Awake()
         {
-            waitTime = startWaitTime;
-            randomSpots = Random.Range(0, spots.Length);
-            agent = GetComponent<NavMeshAgent>();
-            agent.updateRotation = false;
-            agent.updateUpAxis = false;
+            _waitTime = _startWaitTime;
+            NewSpot();
+            _agent.updateRotation = false;
+            _agent.updateUpAxis = false;
         }
 
-        void DestinationRandomSpot()
+        private void FixedUpdate ()
         {
-            agent.SetDestination(spots[randomSpots].position); 
-        }
-
-        void WaitTimeAndDistance()
-        {
-            if (Vector2.Distance(transform.position, spots[randomSpots].position) < 0.2f)
+            if (!IsSteeringTargetClose())
             {
-                if (waitTime <= 0)
-                {
-                    waitTime = startWaitTime;
-                    randomSpots = Random.Range(0, spots.Length);
-                }
-                else
-                {
-                    waitTime -= Time.deltaTime;
-                }
+                _currentViewDirection = (_agent.steeringTarget - transform.position).normalized;
             }
-        }
-
-        private void Update () 
-        {
+            
+            ChasingPlayer();
             DestinationRandomSpot();
             WaitTimeAndDistance();
         }
+
+        private bool IsSteeringTargetClose()
+        {
+            return _agent.steeringTarget == Vector3.zero || Vector2.Distance(transform.position, _agent.steeringTarget) < _setTargetThreshold;
+        }
+
+        private void DestinationRandomSpot()
+        {
+            if (_isChasing) return;
+            
+            _agent.SetDestination(_spots[_randomSpots].position);
+        }
+
+        private void WaitTimeAndDistance()
+        {
+            if (!HasReachedDestination(_spots[_randomSpots].position) || _isChasing) return;
+            
+            if (_waitTime <= 0)
+            {
+                _waitTime = _startWaitTime;
+                NewSpot();
+            }
+            else
+            {
+                _waitTime -= Time.deltaTime;
+            }
+        }
+
+        private void ChasingPlayer()
+        {
+            Collider2D playerCollider = DetectPlayerInCone();
+            
+            if (playerCollider != null)
+            {
+                ChasePlayerWhenInRange(playerCollider);
+            }
+            else if (_isChasing)
+            {
+                GoToLastPositionOfPlayer();
+            }
+        }
+
+        private void ChasePlayerWhenInRange(Collider2D playerCollider)
+        {
+            _playerTransform = playerCollider.transform;
+            _isChasing = true;
+            _agent.SetDestination(_playerTransform.position);
+            _lastPlayerPosition = _playerTransform.position;
+
+            if (!HasReachedDestination(_playerTransform.position)) return;
+            
+            if (_waitTime <= 0)
+            {
+                _waitTime = _startWaitTime;
+                _playerTransform = null;
+                NewSpot();
+                _isChasing = false;
+            }
+            else
+            {
+                _waitTime -= Time.deltaTime;
+            }
+        }
+
+        private void GoToLastPositionOfPlayer()
+        {
+            _agent.SetDestination(_lastPlayerPosition);
+
+            if (!HasReachedDestination(_lastPlayerPosition)) return;
+            
+            if (_waitTime <= 0)
+            {
+                _playerTransform = null;
+                _waitTime = _startWaitTime;
+                NewSpot();
+                _isChasing = false;
+            }
+            else
+            {
+                _waitTime -= Time.deltaTime;
+            }
+        }
+
+        private Collider2D DetectPlayerInCone()
+        {
+            Vector2 position = transform.position;
+            
+            var hit = Physics2D.OverlapCircle(position, _radius, _player);
+
+            if (hit == null) return null;
+            
+            var directionToPlayer = ((Vector2)hit.transform.position - position).normalized;
+            var angleToTarget = Vector2.Angle(_currentViewDirection, directionToPlayer);
+
+            return angleToTarget <= _searchCone ? hit : null;
+        }
+
+        private bool HasReachedDestination(Vector3 destinationPosition)
+        {
+            return Vector2.Distance(transform.position, destinationPosition) < 0.2f;
+        }
+
+        private void NewSpot()
+        {
+            _randomSpots = Random.Range(0, _spots.Length);
+        }
     }
+
+#if UNITY_EDITOR
+    public partial class Patrol
+    {
+        void OnDrawGizmos()
+        {
+            Vector3 position = transform.position;
+            Vector3 dir = transform.rotation * _currentViewDirection.normalized;
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawRay(position, dir * _radius);
+
+            for (var i = -_searchCone ; i <= _searchCone ; i += (_searchCone / 10))
+            {
+                Vector3 ray = Quaternion.AngleAxis(i, Vector3.forward) * dir;
+                Gizmos.DrawRay(position, ray * _radius);
+            }
+        }
+    }
+#endif
 }
