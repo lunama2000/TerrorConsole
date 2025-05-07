90d54cbc26a91a0e0f0e3d888213e75c71717fd4
Chilop <jjuulliiooooooooo@gmail.com>
2025-05-06 18:49:30 -0600

    Refactor AudioManager

--- TerrorConsole/Assets/_Main/Scripts/Audio/IAudioSource.cs
+++ TerrorConsole/Assets/_Main/Scripts/Audio/IAudioSource.cs
@@ -6,13 +6,12 @@ namespace TerrorConsole
     {
         float SFXVolume { get; }
         float MusicVolume { get; }
+    
         void SetSFXVolume(float newVolume);
         void SetMusicVolume(float newVolume);
-        void PlayDoorCloseSFX();
-        void PlayDoorOpenSFX();
-        void PlayPauseSFX();
-        void PlayUIButtonClickSFX();
-        void PlayMusic(MusicType musicType);
+
+        void PlaySFX(string audioKey);
+        void PlayMusic(string musicKey);
         void StopMusic();
         
     }
