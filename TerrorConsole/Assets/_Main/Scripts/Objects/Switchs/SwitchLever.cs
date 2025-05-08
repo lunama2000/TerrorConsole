e2ad8688327cd16bae114366ea1bbb0ab297ef14
lunama2000 <alam.lu.ar@gmail.com>
2025-05-08 15:59:43 -0600

    Redoing the tutorial distribution

--- TerrorConsole/Assets/_Main/Scripts/Objects/Switchs/SwitchLever.cs
+++ TerrorConsole/Assets/_Main/Scripts/Objects/Switchs/SwitchLever.cs
@@ -5,6 +5,7 @@ namespace TerrorConsole
     public class SwitchLever : SwitchObject
     {
         private bool _isPlayerColliding;
+        [SerializeField] Animator _animator;
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
@@ -20,6 +21,12 @@ namespace TerrorConsole
             {
                 InputManager.Source.OnActivateButton1 -= AlternateState;
             }
+        }
+
+        protected override void AlternateState()
+        {
+            base.AlternateState();
+            _animator.SetTrigger("ChangeState");
         }
     }
 }
