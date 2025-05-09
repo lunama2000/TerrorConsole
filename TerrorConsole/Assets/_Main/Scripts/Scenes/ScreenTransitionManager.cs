b3220a9d9e07a6bdf8a4f0286fe42e1059056b21
Chilo <zap520@amerike.edu.mx>
2025-05-08 23:47:12 -0600

    Loading screen functional

--- TerrorConsole/Assets/_Main/Scripts/Scenes/ScreenTransitionManager.cs
+++ TerrorConsole/Assets/_Main/Scripts/Scenes/ScreenTransitionManager.cs
@@ -1,6 +1,7 @@
 using System;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -11,9 +12,11 @@ namespace TerrorConsole
         [Header("COMPONENTS")]
         [SerializeField] private CanvasGroup _mainCanvasGroup;
         [SerializeField] private GameObject _backgroundPanel;
-        
+        [SerializeField] private CanvasGroup _spinner;
+
         [Header("CONFIGURATIONS")]
-        [SerializeField] private float _transitionDuration = 1;
+        [SerializeField] private float _transitionDuration = 1f;
+        [SerializeField] private float _loadingAnimDuration = 0.25f;
 
         public event Action OnTransitionBegan;
         
@@ -61,6 +64,8 @@ namespace TerrorConsole
             _mainCanvasGroup.gameObject.SetActive(true);
             await _mainCanvasGroup.DOFade(1, _transitionDuration).AsyncWaitForCompletion();
             
+            await ShowLoadingIcon();
+            
             onTransition?.Invoke();
             
             await _mainCanvasGroup.DOFade(0, _transitionDuration).AsyncWaitForCompletion();
@@ -76,12 +81,23 @@ namespace TerrorConsole
             _backgroundPanel.transform.localPosition = new Vector3(screenWidth, 0, 0);
             await _backgroundPanel.transform.DOLocalMoveX(0, _transitionDuration).AsyncWaitForCompletion();
             
+            await ShowLoadingIcon();
+
             onTransition?.Invoke();
             
             await _backgroundPanel.transform.DOLocalMoveX(screenWidth, _transitionDuration).AsyncWaitForCompletion();
             _mainCanvasGroup.gameObject.SetActive(false);
         }
-
+        
+        private async UniTask ShowLoadingIcon()
+        {
+            _spinner.gameObject.SetActive(true);
+            await _spinner.DOFade(1, 0.3f).From(0).AsyncWaitForCompletion();
+            await UniTask.Delay(TimeSpan.FromSeconds(_loadingAnimDuration));
+            await _spinner.DOFade(0, 0.3f).AsyncWaitForCompletion();
+            _spinner.gameObject.SetActive(false);
+        }
+        
         private void GoToScene(string sceneName)
         {
             SceneManager.LoadScene(sceneName);
