ef774de2a760f8ed79629782a79be1f26a65666f
Sinkablepeak4 <101901592+Sinkablepeak@users.noreply.github.com>
2025-05-14 20:14:08 -0600

    Notes Work and add script for door open whit notes

---
+++ TerrorConsole/Assets/_Main/Scripts/Objects/Doors/DoorNotesCollected.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace TerrorConsole
+{
+    public class DoorNotesCollected : MonoBehaviour
+    {
+        
+
+    }
+}
