b26de518942646fca85f4547810af0290e1d368a
lunama2000 <alam.lu.ar@gmail.com>
2025-05-26 22:02:26 -0600

    Adding cultists

--- TerrorConsole/Assets/_Main/Scripts/SaveSystem/LevelEventsRecorder.cs
+++ TerrorConsole/Assets/_Main/Scripts/SaveSystem/LevelEventsRecorder.cs
@@ -7,12 +7,18 @@ namespace TerrorConsole
     {
         [SerializeField] string _uniqueEventKey;
 
+        [SerializeField] private bool _deactivateIfLevelEvent;
+
         private void Start()
         {
             if (string.IsNullOrEmpty(_uniqueEventKey))
             {
                 Debug.LogError($"There is no Event Key for {name}, please set a unique Event Key for this item");
                 _uniqueEventKey = name;
+            }
+            else if(_deactivateIfLevelEvent)
+            {
+                gameObject.SetActive(CheckEventState());
             }
         }
 
